c6e69643c2c37173f5471268aae13965b3e27746
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-06-17 20:49:52 +0200

    Query multiple tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,39 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species VARCHAR(255);
+
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+
+-- Add the "species_id" column
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER;
+
+-- Add a foreign key constraint for the "species_id" column
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species (id);
+
+-- Add the "owner_id" column
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER;
+
+-- Add a foreign key constraint for the "owner_id" column
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners (id);
+
+-- Remove the "species" column
+ALTER TABLE animals
+DROP COLUMN species;
