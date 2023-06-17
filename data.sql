e117e0a050103c6d9ad64f266bcd4d229630f30d
IRADUKUNDA Pacific Rugwizangoga <pacific.r.iradukunda@gmail.com>
2023-06-17 18:49:11 +0200

    adds query and update

--- data.sql
+++ data.sql
@@ -15,3 +15,36 @@ VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
 -- Inserting Devimon
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Devimon', '2017-05-12', 5, true, 11);
+
+
+/* Insert more data */
+
+
+-- Inserting Charmander
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Charmander', '2020-02-08', 0, false, -11);
+
+-- Inserting Plantmon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Plantmon', '2021-11-15', 2, true, -5.7);
+
+-- Inserting Squirtle
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Squirtle', '1993-04-02', 3, false, -12.13);
+
+-- Inserting Angemon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Angemon', '2005-06-12', 1, true, -45);
+
+-- Inserting Boarmon
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Boarmon', '2005-06-07', 7, true, 20.4);
+
+-- Inserting Blossom
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Blossom', '1998-10-13', 3, true, 17);
+
+-- Inserting Ditto
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES ('Ditto', '2022-05-14', 4, true, 22);
+
